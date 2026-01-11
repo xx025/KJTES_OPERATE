28ca73db7b79916db431b652899ee795fe07b386
xx025 <71559822+xx025@users.noreply.github.com>
2026-01-12 01:14:54 +0800

    Add chessboard configuration and search results for strategy evaluation
    - Created a new chessboard configuration file with initial state.
    - Added a JSON file containing search results, including best strategies, steps, and various strategy evaluations.

--- test_ocr_clicker.py
+++ test_ocr_clicker.py
@@ -1,10 +1,11 @@
+from time import sleep
+import numpy as np
 from modules.ocrclicker import OCRClicker
 from modules.ppdocr import ocr_recognition
+from pycvt import draw_bounding_boxes
 
-bot = OCRClicker(
-    window_title="开局托儿所",
-    ocr_func=ocr_recognition
-)
 
-bot.click(["再来一局","再次挑战"], timeout=5)
-print("检测到再来一局，开始下一轮操作...")
+import imageio.v3 as iio
+
+
+
